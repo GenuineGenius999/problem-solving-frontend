1cd257170ae7387ab114a33fa7df0b443c899780
GenuineGenius999 <lm1876356@gmail.com>
2026-01-16 12:20:15 +0900

    updated: backend url

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ function App() {
 
     try {
       // const res = await fetch("http://localhost:8080/api/solve", {
-      const res = await fetch("https://d619c742c02a.ngrok-free.app/api/solve", {
+      const res = await fetch("https://e945911356ce.ngrok-free.app/api/solve", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
